ff5b8b81f77114b9104c996e2377797f1838e52d
Navendu Singh <54157440+NavenduSingh07@users.noreply.github.com>
2024-05-06 10:56:01 +0530

    some file deleted

--- forms/sendEmail.js
+++ forms/sendEmail.js
@@ -1,37 +1,37 @@
 // Select the form element
-const form = document.getElementById('contact-form');
+// const form = document.getElementById('contact-form');
 
 // Add event listener for form submission
-form.addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent default form submission behavior
+// form.addEventListener('submit', function(event) {
+//     event.preventDefault(); // Prevent default form submission behavior
 
     // Fetch form data
-    const formData = new FormData(this);
+    // const formData = new FormData(this);
 
     // Send form data to backend
-    fetch('/sendEmail.php', { // Assuming your PHP script is named sendEmail.php
-        method: 'POST',
-        body: formData
-    })
-    .then(response => {
+    // fetch('/sendEmail.php', { // Assuming your PHP script is named sendEmail.php
+    //     method: 'POST',
+    //     body: formData
+    // })
+    // .then(response => {
         // Check if the response is successful
-        if (response.ok) {
+        // if (response.ok) {
             // Show success message
-            document.querySelector('.sent-message').style.display = 'block';
+            // document.querySelector('.sent-message').style.display = 'block';
             // Reset form fields
-            form.reset();
-        } else {
+        //     form.reset();
+        // } else {
             // Show error message
-            document.querySelector('.error-message').style.display = 'block';
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
+    //         document.querySelector('.error-message').style.display = 'block';
+    //     }
+    // })
+    // .catch(error => {
+    //     console.error('Error:', error);
         // Show error message
-        document.querySelector('.error-message').style.display = 'block';
-    })
-    .finally(() => {
+    //     document.querySelector('.error-message').style.display = 'block';
+    // })
+    // .finally(() => {
         // Hide loading message
-        document.querySelector('.loading').style.display = 'none';
-    });
-});
+//         document.querySelector('.loading').style.display = 'none';
+//     });
+// });
